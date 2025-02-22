f87c24f34a5a30530e8f1d37ee97d7b2c5ff5c7f
Sophie Chou <156702779+sophiechou1@users.noreply.github.com>
2025-02-22 12:26:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 /* script.js - Sophie Chou */
 
 // function to open email and email me
-window.onload = function() {
-    // don't show if not on home page
-    if (window.location.href.includes('index.html')) {
-        // create new button element
-        var button = document.createElement('a');
-        // email link
-        button.href = 'mailto:schou2@nd.edu';
-        // bootstrap button
-        button.className = 'btn custom-button mx-2';
-        button.innerText = 'Email Me!';
+// window.onload = function() {
+//     // don't show if not on home page
+//     if (window.location.href.includes('index.html')) {
+//         // create new button element
+//         var button = document.createElement('a');
+//         // email link
+//         button.href = 'mailto:schou2@nd.edu';
+//         // bootstrap button
+//         button.className = 'btn custom-button mx-2';
+//         button.innerText = 'Email Me!';
 
-        // place button on home page
-        var container = document.querySelector('.container.text-center.my-4');
+//         // place button on home page
+//         var container = document.querySelector('.container.text-center.my-4');
 
-        // append button to container
-        container.appendChild(button);
-    }
-}
+//         // append button to container
+//         container.appendChild(button);
+//     }
+// }
 
 // open resume in new tab if button is clicked
 document.getElementById("viewResumeBtn").addEventListener("click", function() { 
     window.open("/assets/Sophie Chou - Resume.pdf", "_blank");
+});
+
+document.getElementById("email").addEventListener("click", function() { 
+    button.href = 'mailto:schou2@nd.edu';
 });
